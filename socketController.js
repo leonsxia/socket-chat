9cd2c8a723c16c272e50314e4039d97dac7f3abf
leonsxia <leon_s_xia@126.com>
2019-03-22 17:03:38 +0800

    modify socketController.js

--- socketController.js
+++ socketController.js
@@ -3,14 +3,15 @@ var logHelper = require('./logHelper'),
     socketHandlers = require('./socketHandlers');
 
 function SocketController() {
-    var _father = new socketHandlers();
+    // var _father = new socketHandlers();
+    socketHandlers.call(this);
+    var _father = this;
     return {
         init: function(io, users) {            
             _father.init(io, users);
             return this;
         },
         route: function() {
-            var that = this;
             _father.io.on('connection', function(socket) { // listen on connection event for incoming sockets
                 socket.haslogin = false;
                 var handlers = _father.socketHandlers;
